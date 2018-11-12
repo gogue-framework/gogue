9990c910d1dba7bf143e97750fe62ef8e2c5c1ae
jcerise <jcerise06@gmail.com>
2018-11-11 17:22:47 -0700

    Added a few convenient methods to screen management, including deleting a screen from the manager, and transitioning screens via a screen object, rather than a name (for example, for navigating to previous screens dynamically)

--- screens/screen.go
+++ screens/screen.go
@@ -35,7 +35,36 @@ func (sm *ScreenManager) AddScreen(screenName string, screen Screen) {
 	}
 }
 
-func (sm *ScreenManager) SetScreen(screenName string) {
+// RemoveScreen will remove a screen from the ScreenManager. This can be useful when a temporary screen needs to be
+// created, as it can be quickly added (rather than registering at game creation), and then removed when it is no
+// longer needed
+func (sm *ScreenManager) RemoveScreen(screenName string, screen Screen) {
+	// Check if the given screenName exists in the ScreenManager
+	if _, ok := sm.Screens[screenName]; ok {
+		delete(sm.Screens, screenName)
+	} else {
+		// A screen with the given name does not exist
+		fmt.Printf("A screen with name %v was not found on ScreenManager %v!", screenName, sm)
+	}
+}
+
+// SetScreen will set the current screen property of the screen manager to the provided screen
+func (sm *ScreenManager) SetScreen(screen Screen) {
+	// Call the exit function of the currentScreen, and set the currentScreen as the previousScreen
+	// Only do this if there is a currentScreen
+	if sm.CurrentScreen != nil {
+		sm.CurrentScreen.Exit()
+		sm.PreviousScreen = sm.CurrentScreen
+	}
+
+	// Set the provided screen as the currentScreen, and call the enter() function of the new currentScreen
+	sm.CurrentScreen = screen
+	sm.CurrentScreen.Enter()
+}
+
+// SetScreenByName takes a string representing the screen desired to navigate to. It will then transition the
+// ScreenManager to the specified screen, if one is found.
+func (sm *ScreenManager) SetScreenByName(screenName string) {
 	// Check if the given screenName exists in the ScreenManager
 	if _, ok := sm.Screens[screenName]; ok {
 		// Call the exit function of the currentScreen, and set the currentScreen as the previousScreen
