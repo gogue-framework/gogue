6af58d9ffe6a2832406c06316257b5b4a9ed7f59
jcerise <jcerise06@gmail.com>
2018-12-26 16:10:13 -0700

    Making everything in the system message code public.

--- ecs/systemsMessages.go
+++ ecs/systemsMessages.go
@@ -1,13 +1,13 @@
 package ecs
 
 type SystemMessageType struct {
-	name string
-	creatingSystem System
+	Name string
+	CreatingSystem System
 }
 
 type SystemMessage struct {
-	messageType SystemMessageType
-	messageContent map[string]string
+	MessageType SystemMessageType
+	MessageContent map[string]string
 }
 
 // SystemMessageQueue is a super simple way of messaging between systems. Essentially, it is nothing more than a list of
@@ -18,24 +18,24 @@ type SystemMessage struct {
 // be cleared out occasionally, either by the subscribing systems, or the game loop. Pretty simple for now, but should
 // solve a subset of problems nicely.
 type SystemMessageQueue struct {
-	messages []SystemMessage
+	Messages []SystemMessage
 }
 
 // BroadcastMessage appends a system message onto the games SystemMessageQueue, allowing it to consumed by a service
 // subscribes to the MessageType.
 // TODO: This name is a little misleading, as it doesn't actually broadcast, so much as append to a list for consumption
-func (smq *SystemMessageQueue) broadcastMessage(messageType SystemMessageType, messageContent map[string]string) {
-	newMessage := SystemMessage{messageType: messageType, messageContent: messageContent}
+func (smq *SystemMessageQueue) BroadcastMessage(messageType SystemMessageType, messageContent map[string]string) {
+	newMessage := SystemMessage{MessageType: messageType, MessageContent: messageContent}
 
-	smq.messages = append(smq.messages, newMessage)
+	smq.Messages = append(smq.Messages, newMessage)
 }
 
 // GetMessagesOfType returns a list of SystemMessages that have messageType. Can return an empty list
 func (smq *SystemMessageQueue) GetMessagesOfType(messageType SystemMessageType) []SystemMessage {
 	messages := []SystemMessage{}
 
-	for _, message := range smq.messages {
-		if message.messageType == messageType {
+	for _, message := range smq.Messages {
+		if message.MessageType == messageType {
 			messages = append(messages, message)
 		}
 	}
