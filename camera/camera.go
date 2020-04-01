dd14fce157932f44e2699aef9465977d8ee254b8
jcerise <jcerise06@gmail.com>
2020-03-31 23:31:52 -0600

    Added tests for the Camera package, as well as some docstrings for Camera methods

--- camera/camera.go
+++ camera/camera.go
@@ -1,5 +1,7 @@
 package camera
 
+import "errors"
+
 type GameCamera struct {
 	X      int
 	Y      int
@@ -7,6 +9,32 @@ type GameCamera struct {
 	Height int
 }
 
+// NewGameCamera creates a new GameCamera instance. (x, y) are the starting location for the camera viewport. The
+// viewport will be centered over these coordinates on the GameMap. width and height designate the size of the
+// cameras viewport, or how much of the GameMap is shown. If the viewport is the same size as, or larger than the
+// GameMap, the entire map will be shown. If the viewport is smaller than the GameMap, only the portion of the GameMap
+// that fits within the centered viewport will be shown.
+func NewGameCamera(x, y, width, height int) (*GameCamera, error) {
+	gameCamera := GameCamera{}
+
+	if x < 0 || y < 0 || width < 0 || height < 0 {
+		err := errors.New("GameCamera position values must be greater than 0")
+		return nil, err
+	}
+
+	gameCamera.X = x
+	gameCamera.Y = y
+	gameCamera.Width = width
+	gameCamera.Height = height
+
+	return &gameCamera, nil
+}
+
+// MoveCamera centers the GameCamera viewport to the new location specified over the GameMap. If the viewport is
+// larger than the GameMap, the viewport is not moved. If the viewport is the same size as the GameMap, the viewport
+// is not moved. If the GameMap is larger than the viewport, and coordinates outside of the viewport are requested,
+// the viewport is centered over the new coordinate set. The viewport cannot be centered outside the bounds of the
+// GameMap.
 func (c *GameCamera) MoveCamera(targetX int, targetY int, mapWidth int, mapHeight int) {
 	// Update the camera coordinates to the target coordinates
 	x := targetX - c.Width/2
@@ -31,6 +59,11 @@ func (c *GameCamera) MoveCamera(targetX int, targetY int, mapWidth int, mapHeigh
 	c.X, c.Y = x, y
 }
 
+// ToCameraCoordinates translates a GameMap coordinate pair to a GameCamera coordinate pair. GameMap coordinates and
+// Camera coordinates are two different things. A GameMap coordinate pair designates a location on the GameMap, whereas
+// a Camera coordinate pair designates a location within the cameras viewport. ToCameraCoordinates translates a GameMap
+// coordinate pair to a Camera coordinate pair. If the map coordinate pair is outside of the viewport of the camera,
+// (-1, -1) is returned to indicate this
 func (c *GameCamera) ToCameraCoordinates(mapX int, mapY int) (cameraX int, cameraY int) {
 	// Convert coordinates on the map, to coordinates on the viewport
 	x, y := mapX-c.X, mapY-c.Y
---
+++ camera/camera_test.go
@@ -0,0 +1,74 @@
+package camera
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewCamera(t *testing.T) {
+	gameCamera, err := NewGameCamera(0, 1 ,2 ,3)
+
+	assert.NotNil(t, gameCamera)
+	assert.Nil(t, err)
+	assert.Equal(t, gameCamera.X, 0)
+	assert.Equal(t, gameCamera.Y, 1)
+	assert.Equal(t, gameCamera.Width, 2)
+	assert.Equal(t, gameCamera.Height, 3)
+
+	// Check to ensure values < 0 are not allowed
+	gameCamera, err = NewGameCamera(-1, 0, 1, 2)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, gameCamera)
+}
+
+func TestGameCamera_MoveCamera(t *testing.T) {
+	// Set up a new camera, with a viewport of 100x100
+	gameCamera, _ := NewGameCamera(0, 0, 100, 100)
+
+	// Move the camera to a location that is already within the cameras viewport. This effectively will not
+	// move the viewport
+	gameCamera.MoveCamera(25, 25, 100, 100)
+	assert.Equal(t, gameCamera.X, 0)
+	assert.Equal(t, gameCamera.Y, 0)
+
+	// Move the camera to a location that is more than halfway across the map, and the cameras viewport, on a small
+	// map. This should not move the camera, since the entire map is within the cameras viewport
+	gameCamera.MoveCamera(51, 51, 100, 100)
+	assert.Equal(t, gameCamera.X, 0)
+	assert.Equal(t, gameCamera.Y, 0)
+
+	// Move the camera to a location that is outside of its viewport (on a larger map). This should update the
+	// the cameras coordinates
+	gameCamera.MoveCamera(101, 101, 500, 500)
+	assert.Equal(t, gameCamera.X, 51)
+	assert.Equal(t, gameCamera.Y, 51)
+}
+
+func TestGameCamera_ToCameraCoordinates(t *testing.T) {
+	// GameMap coordinates and Camera coordinates are two different things. A GameMap coordinate pair designates a
+	// a location on the GameMap, whereas a Camera coordinate pair designates a location within the cameras viewport.
+	// ToCameraCoordinates translates a GameMap coordinate pair to a Camera coordinate pair. If the map coordinate pair
+	// is outside of the viewport of the camera, (-1, -1) is returned to indicate this
+	gameCamera, _ := NewGameCamera(0, 0, 100, 100)
+
+	// With the camera viewport matching the top left corner of the map, the coordinates should be the same for both
+	cameraX, cameraY := gameCamera.ToCameraCoordinates(2, 2)
+	assert.Equal(t, cameraX, 2)
+	assert.Equal(t, cameraY, 2)
+
+	// If the camera is moved beyond the top left corner of the map, the two coordinate systems no longer align
+	gameCamera.MoveCamera(50, 70, 500, 500)
+	cameraX, cameraY = gameCamera.ToCameraCoordinates(40, 50)
+	assert.Equal(t, cameraX, 40)
+	assert.Equal(t, cameraY, 30)
+
+	// If a map coordinate pair that is outside of the viewport of the camera is provided, (-1, -1) should be returned
+	cameraX, cameraY = gameCamera.ToCameraCoordinates(1, 1)
+	assert.Equal(t, cameraX, -1)
+	assert.Equal(t, cameraY, -1)
+
+	cameraX, cameraY = gameCamera.ToCameraCoordinates(499, 499)
+	assert.Equal(t, cameraX, -1)
+	assert.Equal(t, cameraY, -1)
+}
