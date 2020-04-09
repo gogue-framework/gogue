7a06b648fcb04fdd62556c514bf63d710dcf1104
jcerise <jcerise06@gmail.com>
2020-04-09 15:36:32 -0600

    Added a root go file so the project will build.

---
+++ gogue.go
@@ -0,0 +1,5 @@
+package gogue
+
+// This is silly, but it seems you can't build a package without a go file in the root of the package. I'm sure there's
+// something I'm not aware of/mis-understanding here, but until such time as I figure that out, I guess this file will
+// stay here.
